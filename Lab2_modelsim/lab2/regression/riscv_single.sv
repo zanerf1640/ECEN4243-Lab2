8b8cb100ad474d18a67d91c9a319e859e0c7e58d
zanef <zane.francis@cox.net>
2026-03-20 21:50:13 -0500

    Fixed a bug with the J type instructions, now supports all instructions.

--- Lab2_modelsim/lab2/regression/riscv_single.sv
+++ Lab2_modelsim/lab2/regression/riscv_single.sv
@@ -321,7 +321,7 @@ module extend (input  logic [31:7] instr,
        // B−type (branches)
        3'b010:  immext = {{20{instr[31]}}, instr[7], instr[30:25], instr[11:8], 1'b0};       
        // J−type (jal)
-       3'b111:  immext = {{12{instr[31]}}, instr[19:12], instr[20], instr[30:21], 1'b0};
+       3'b011:  immext = {{12{instr[31]}}, instr[19:12], instr[20], instr[30:21], 1'b0};
        // U−type (lui, auipc)
        3'b100:  immext = {instr[31:12], 12'b0};
        default: immext = 32'bx; // undefined
